31434e8f85732d2ce89c0cc0bd49530b4a560e2c
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2021-06-09 11:24:29 -0600

    Multiply Array Elements Algo Done

---
+++ reduceButGrow.js
@@ -0,0 +1,22 @@
+// 8kyu
+
+// multiply all items in an array of numbers and return the sum.
+
+const grow = (x) => {
+  // Define sum. Since we're mutiplying, use 1 so as to not return 0.
+  let sum = 1;
+  // Loop through the array
+  for (let i=0; i < x.length; i++) {
+    // Do the maths. Sum equals sum times each element.
+    sum *= x[i]
+  }
+  // Out of the loop, return the total value of Sum.
+  return sum
+};
+
+// Call the function using [1,2,3,4]
+// Expected output: 24
+
+console.log(
+  grow([1,2,3,4])
+);
