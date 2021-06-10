77522e315a0b39969b0d3b85491f45c666bb5f14
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2021-06-09 20:37:29 -0600

    Sum of Digits Algorithm finished

---
+++ sumDigits.js
@@ -0,0 +1,17 @@
+// 7 kyu
+
+// Write a function named sumDigits which takes a number as input    and returns the sum of the absolute value of each of the number's decimal digits.
+
+
+function sumDigits(number) {
+  // set the absolutle value of number to a variable.
+  let math = Math.abs(number)
+  // convert number to string, split to an array, map to transform into number, add together.
+  let num = math.toString().split('').map(Number).reduce((a,b) => a+b);
+  return num
+}
+
+// Call function with number parameter.
+console.log(
+  sumDigits(-82)
+);
