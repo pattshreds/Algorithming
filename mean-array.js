6db9e87ffcca5831f522a2c00626b0eb2ebfc554
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2021-06-24 10:13:52 -0600

    find mean done.

---
+++ mean-array.js
@@ -0,0 +1,12 @@
+//8 kyu
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+//======================================================================================
+
+const getAverage = (marks) =>
+    Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+
+console.log(getAverage([1, 2, 3, 4, 5]));
